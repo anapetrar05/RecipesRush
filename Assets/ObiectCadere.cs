af6bcf01876e0cbae95d31861c6dc6517c86c0a0
Petrar Anamaria Alvina <anapetrar05@gmail.com>
2026-06-09 00:03:41 +0300

    Clean up comments in ObiectCadere.cs
    Removed commented-out code for clarity.

--- Assets/ObiectCadere.cs
+++ Assets/ObiectCadere.cs
@@ -6,15 +6,13 @@ public class ObiectCadere : MonoBehaviour
 
     void Update()
     {
-        // Ingredientele cad constant în jos
+        
         transform.Translate(Vector3.down * viteza * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // LĂSĂM COȘUL SĂ SE OCUPE DE CAZUL 1 (când îl prinde, îl distruge el din scriptul playerControler)
-
-        // Cazul 2: Dacă atinge banda roz de jos (PlatformaJos), dispare
+        
         if (collision.CompareTag("PlatformaJos"))
         {
             Destroy(gameObject);
